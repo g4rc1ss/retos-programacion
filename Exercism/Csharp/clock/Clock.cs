03936edb16e843482876b5304cebc5c16be613b4
Asier Garcia <a.garciabarrenengoa@dxc.com>
2024-02-27 12:38:42 +0100

    feat: :sparkles: Add Clock

--- Exercism/Csharp/clock/Clock.cs
+++ Exercism/Csharp/clock/Clock.cs
@@ -1,19 +1,41 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 
-public class Clock
+public class Clock : IEquatable<Clock>
 {
+    private TimeOnly time;
+
     public Clock(int hours, int minutes)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var timeSpan = new TimeSpan(hours, minutes, 0);
+        time = timeSpan.Ticks < 0 ? TimeOnly.MaxValue : new TimeOnly();
+        time = time.Add(timeSpan).Add(TimeSpan.FromTicks(1));
+    }
+
+    public Clock(TimeOnly time)
+    {
+        this.time = time;
     }
 
     public Clock Add(int minutesToAdd)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var addTime = time.Add(TimeSpan.FromMinutes(minutesToAdd));
+        return new Clock(addTime);
+    }
+
+    public bool Equals(Clock other)
+    {
+        return other.ToString().Equals(ToString());
     }
 
     public Clock Subtract(int minutesToSubtract)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var subtractTime = time.Add(TimeSpan.FromMinutes(minutesToSubtract * -1));
+        return new Clock(subtractTime);
     }
+    public override string ToString() =>
+        time.Add(TimeSpan.FromTicks(1))
+        .ToString(@"HH:mm");
 }
