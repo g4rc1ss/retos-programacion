c9063b8acb94123f95b8873e744b0078f75fafb9
Asier Garcia <a.garciabarrenengoa@dxc.com>
2024-02-28 21:30:13 +0100

    feat: :sparkles: Add Parallel exercis

--- Exercism/Csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
+++ Exercism/Csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -1,10 +1,47 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 public static class ParallelLetterFrequency
 {
     public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
     {
-        throw new NotImplementedException();
+        var response = new Dictionary<char, int>();
+        var lockContainsObject = new object();
+        var lockNewObject = new object();
+
+        Parallel.ForEach(texts, new ParallelOptions() { MaxDegreeOfParallelism = 100 },
+            item =>
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            var lowerItem = item.ToLower();
+            foreach (var character in lowerItem)
+            {
+                if (Regex.IsMatch(character.ToString(), @"(\s|,|\d)"))
+                {
+                    continue;
+                }
+                if (response.ContainsKey(character))
+                {
+                    lock (lockContainsObject)
+                    {
+                        response[character] = ++response[character];
+                    }
+                }
+                else
+                {
+                    lock (lockNewObject)
+                    {
+                        response.Add(character, 1);
+                    }
+                }
+            }
+        });
+
+        return response.ToDictionary();
     }
 }
