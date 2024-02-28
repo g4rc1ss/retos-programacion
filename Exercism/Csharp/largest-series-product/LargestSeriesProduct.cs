68701623e105602110d1845e37c2afc0d66bde47
Asier Garcia <a.garciabarrenengoa@dxc.com>
2024-02-28 20:16:34 +0100

    feat: :sparkles:

--- Exercism/Csharp/largest-series-product/LargestSeriesProduct.cs
+++ Exercism/Csharp/largest-series-product/LargestSeriesProduct.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class LargestSeriesProduct
 {
-    public static long GetLargestProduct(string digits, int span) 
+    public static long GetLargestProduct(string digits, int span)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        CheckInput(digits, span);
+        var listResult = new List<long>();
+        for (int i = 0; i < digits.Length; i++)
+        {
+            var maxIndex = i + span;
+            if (maxIndex > digits.Length)
+            {
+                break;
+            }
+            var numbers = digits[i..maxIndex]
+                .Select(x => int.Parse(new ReadOnlySpan<char>(in x)));
+
+            long result = 1;
+            foreach (var item in numbers)
+                result *= item;
+
+            listResult.Add(result);
+        }
+        return listResult
+            .Max();
+    }
+
+    private static void CheckInput(string digits, int span)
+    {
+        if (span <= digits.Length && span > 0
+        && !string.IsNullOrEmpty(digits) 
+        // Busca caracteres no numericos
+        && !Regex.IsMatch(digits, @"(\D)"))
+        {
+            return;
+        }
+        throw new ArgumentException();
     }
 }
--- Exercism/Csharp/simple-cipher/SimpleCipher.cs
+++ Exercism/Csharp/simple-cipher/SimpleCipher.cs
@@ -1,32 +1,68 @@
 using System;
+using System.Linq;
+using System.Text;
 
 public class SimpleCipher
 {
+    private readonly string _abc = "abcdefghijklmnopqrstuvwxyz";
+    private readonly string _key;
+
     public SimpleCipher()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        _key = new string(Enumerable.Range(0, 10).Select(x => 'a').ToArray());
     }
 
     public SimpleCipher(string key)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        _key = key;
     }
-    
-    public string Key 
-    {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+
+    public string Key => _key;
 
     public string Encode(string plaintext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var result = new StringBuilder();
+        var keyIndex = 0;
+        foreach (var character in plaintext)
+        {
+            var indexMinus = _abc.IndexOf(character);
+            if (indexMinus == -1)
+            {
+                result.Append(character);
+                continue;
+            }
+
+            if (indexMinus != -1)
+            {
+                result.Append(_abc[(indexMinus + _abc.IndexOf(Key[keyIndex])) % _abc.Length]);
+            }
+            keyIndex++;
+            keyIndex = keyIndex < Key.Length ? keyIndex : 0;
+        }
+        return result.ToString();
     }
 
     public string Decode(string ciphertext)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var result = new StringBuilder();
+        var keyIndex = 0;
+        foreach (var character in ciphertext)
+        {
+            var indexMinus = _abc.IndexOf(character);
+            if (indexMinus == -1)
+            {
+                result.Append(character);
+                continue;
+            }
+
+            if (indexMinus != -1)
+            {
+                var indexToDecrypt = (indexMinus - _abc.IndexOf(Key[keyIndex])) % _abc.Length;
+                result.Append(_abc[indexToDecrypt < 0 ? _abc.Length - Math.Abs(indexToDecrypt) : indexToDecrypt]);
+            }
+            keyIndex++;
+            keyIndex = keyIndex < Key.Length ? keyIndex : 0;
+        }
+        return result.ToString();
     }
 }
