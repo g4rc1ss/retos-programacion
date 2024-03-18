41846b2e7f39ef2ea55869f202c6f8727fa2b11d
Asier Garcia <a.garciabarrenengoa@dxc.com>
2024-03-18 15:58:32 +0100

    feat: :sparkles: Add Parallel-letter-frequency rust

--- Exercism/Rust/parallel-letter-frequency/src/lib.rs
+++ Exercism/Rust/parallel-letter-frequency/src/lib.rs
@@ -1,17 +1,22 @@
 use std::{
-    collections::HashMap, sync::{Arc, Mutex}, thread::{self, JoinHandle, Thread}
+    collections::HashMap,
+    sync::{Arc, Mutex},
+    thread::{self},
 };
 
 use regex::Regex;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let return_map = Mutex::new(HashMap::new());
+    let return_map = Arc::new(Mutex::new(HashMap::new()));
     let mut handle = vec![];
 
     for text in input {
+        let return_map_clone = Arc::clone(&return_map);
+        let text = (*text).to_string();
+
         if handle.len() < worker_count {
             let thread_handle = thread::spawn(move || {
-                count_character(&text, &mut return_map.lock().unwrap());
+                count_character(&text, &mut return_map_clone.lock().unwrap());
             });
             handle.push(thread_handle);
         } else {
@@ -23,22 +28,17 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
         worker.join().unwrap();
     }
 
-    HashMap::new()
+    let return_map: HashMap<char, usize> = return_map.lock().unwrap().clone();
+    return_map
 }
 
-fn count_character(text: &&str, return_map: &mut HashMap<char, usize>) {
+fn count_character(text: &str, return_map: &mut HashMap<char, usize>) {
     let lower_text = text.to_lowercase();
     'characters_for: for character in lower_text.chars() {
         let regex = Regex::new(r"(\s|,|\d)").unwrap();
         if regex.is_match(&character.to_string()) {
             continue 'characters_for;
         }
-        if return_map.contains_key(&character) {
-            if let Some(value) = return_map.get_mut(&character) {
-                *value += 1;
-            }
-        } else {
-            return_map.insert(character, 1);
-        }
+        *return_map.entry(character).or_insert(0) += 1;
     }
 }
