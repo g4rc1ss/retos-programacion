1a78bc539c7834046dd804148de2d332601a3761
Asier <asiergarciab@outlook.com>
2024-12-17 11:36:54 +0100

    Exercise 2 part 2 Advent of code 2022

--- AdventOfCode/Advent-of-code-2022/_02_rock_paper_scissors/src/Exercise.ts
+++ AdventOfCode/Advent-of-code-2022/_02_rock_paper_scissors/src/Exercise.ts
@@ -2,39 +2,27 @@ import fs from "fs/promises";
 
 
 export class Exercise {
+    rivalGame: { [clave: string]: Game } = {
+        A: Game.Rock,
+        B: Game.Paper,
+        C: Game.Scissors,
+    };
+
+    playerGame: { [clave: string]: Game } = {
+        X: Game.Rock,
+        Y: Game.Paper,
+        Z: Game.Scissors,
+    };
+
 
     async part1(): Promise<number> {
         let finalResult: number = 0;
         let text = await this.readFileAsync();
-        let rivalGame: { [clave: string]: Game } = {
-            A: Game.Rock,
-            B: Game.Paper,
-            C: Game.Scissors,
-        };
-
-        let playerGame: { [clave: string]: Game } = {
-            X: Game.Rock,
-            Y: Game.Paper,
-            Z: Game.Scissors,
-        };
-
-        const scoreOpt = (gameOpt: Game) => {
-            switch (gameOpt) {
-                case Game.Rock:
-                    return 1;
-                case Game.Paper:
-                    return 2;
-                case Game.Scissors:
-                    return 3;
-                default:
-                    return 0;
-            }
-        };
 
         for (const line of text) {
             let options = line.split(" ");
-            let rivalOpt = rivalGame[options[0]]
-            let playerOpt = playerGame[options[1]];
+            let rivalOpt = (this.rivalGame)[options[0]]
+            let playerOpt = (this.playerGame)[options[1]];
             let result = this.isPlayerWin(rivalOpt, playerOpt);
 
 
@@ -46,15 +34,72 @@ export class Exercise {
                     finalResult += 6;
                     break;
             }
-            finalResult += scoreOpt(playerOpt);
+            finalResult += this.scoreOpt(playerOpt);
         }
 
         return finalResult;
     }
 
     async part2() {
+        let finalResult: number = 0;
         let text = await this.readFileAsync();
 
+        for (const line of text) {
+            let options = line.split(" ");
+            let rivalOpt = (this.rivalGame)[options[0]]
+            let playerOpt = options[1];
+
+            let playerGame: Game = Game.Paper;
+
+            const checkPlayerWins = (playerOpt: string, rivalOpt: Game) => {
+                if (playerOpt == "X") {
+                    playerGame = rivalOpt == Game.Paper
+                        ? Game.Rock : rivalOpt == Game.Rock
+                            ? Game.Scissors : rivalOpt == Game.Scissors
+                                ? Game.Paper : Game.Paper;
+                    return GameResult.defeat;
+                }
+                if (playerOpt == "Y") {
+                    playerGame = rivalOpt;
+                    return GameResult.draw;
+
+                }
+                if (playerOpt == "Z") {
+                    playerGame = rivalOpt == Game.Paper
+                        ? Game.Scissors : rivalOpt == Game.Rock
+                            ? Game.Paper : rivalOpt == Game.Scissors
+                                ? Game.Rock : Game.Paper;
+                    return GameResult.Win;
+                }
+            };
+
+            let result = checkPlayerWins(playerOpt, rivalOpt);
+
+            switch (result) {
+                case GameResult.draw:
+                    finalResult += 3;
+                    break;
+                case GameResult.Win:
+                    finalResult += 6;
+                    break;
+            }
+            finalResult += this.scoreOpt(playerGame);
+        }
+
+        return finalResult;
+    }
+
+    private scoreOpt(gameOpt: Game): number {
+        switch (gameOpt) {
+            case Game.Rock:
+                return 1;
+            case Game.Paper:
+                return 2;
+            case Game.Scissors:
+                return 3;
+            default:
+                return 0;
+        }
     }
 
     private isPlayerWin(rival: Game, player: Game): GameResult {
