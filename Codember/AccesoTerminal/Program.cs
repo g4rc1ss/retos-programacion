0ebc7a400ce4f724fbc5f7c44457dd6f523352b9
Asier <asiergarciab@outlook.com>
2025-07-01 09:32:15 +0200

    feat: add AccesoTerminal

---
+++ Codember/AccesoTerminal/Program.cs
@@ -0,0 +1,31 @@
+﻿const string Combination = "URDURUDRUDLLLLUUDDUDUDUDLLRRRR";
+const string numbers = "528934712834";
+
+List<int> finalNumbers = [.. numbers.Select(x => int.Parse(x.ToString()))];
+
+int index = 0;
+foreach (char c in Combination)
+{
+    switch (c)
+    {
+        case 'R':
+            index += 1 % finalNumbers.Count;
+            break;
+        case 'L':
+            index = ((index - 1) + finalNumbers.Count) % finalNumbers.Count;
+            break;
+        case 'U':
+            finalNumbers[index] = (finalNumbers[index] + 1) % 10;
+            break;
+        case 'D':
+            finalNumbers[index] = (finalNumbers[index] - 1 + 10) % 10;
+            break;
+        default:
+            break;
+    }
+}
+
+foreach (int finalNumber in finalNumbers)
+{
+    Console.Write(finalNumber);
+}
