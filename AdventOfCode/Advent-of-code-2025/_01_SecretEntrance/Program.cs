6fec65fe35a433a4a562c46e6c361e0b1e9790ac
Asier <asiergarciab@outlook.com>
2025-12-31 15:12:59 +0100

    feat: advent of code

--- AdventOfCode/Advent-of-code-2025/_01_SecretEntrance/Program.cs
+++ AdventOfCode/Advent-of-code-2025/_01_SecretEntrance/Program.cs
@@ -1,9 +1,8 @@
 string[] text = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"];
 
-text = await File.ReadAllLinesAsync("01-Secret_Entrance/text.txt");
-int password = 0;
+// text = await File.ReadAllLinesAsync("./text.txt");
 
-password = Part1(text);
+int password = Part1(text);
 Console.WriteLine($"Password: {password}");
 
 password = Part2(text);
@@ -49,43 +48,26 @@ static int Part2(string[] text)
     foreach (string line in text)
     {
         int value = int.Parse(line[1..]);
-        bool crossesZero = false;
 
         switch (line[0])
         {
             case 'L':
-                crossesZero = CrossesZero(number, -value);
+                password += CountZeroCrossings(number, -value);
                 number = CalculateDial(number, -value);
                 break;
             case 'R':
-                crossesZero = CrossesZero(number, value);
+                password += CountZeroCrossings(number, value);
                 number = CalculateDial(number, value);
                 break;
-            default:
-                break;
-        }
-        if (number == 0 || crossesZero)
-        {
-            password++;
         }
     }
 
     return password;
 }
 
-static bool CrossesZero(int start, int delta, int mod = 100)
+static int CountZeroCrossings(int start, int delta, int mod = 100)
 {
-    int sum = start + delta;
+    int b = start + delta;
 
-    if (sum == 0)
-    {
-        return false;
-    }
-
-    if (sum >= mod || sum < 0)
-    {
-        return true;
-    }
-
-    return false;
+    return Math.Abs(b / mod - start / mod);
 }
